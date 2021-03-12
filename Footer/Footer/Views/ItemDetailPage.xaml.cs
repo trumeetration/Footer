4c270cb09cb09e2f3a1d0c5f820b716a5e1a9471
trum <trumeetration@gmail.com>
2021-03-12 23:49:02 +0500

    Добавьте файлы проекта.

---
+++ Footer/Footer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Footer.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Footer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
