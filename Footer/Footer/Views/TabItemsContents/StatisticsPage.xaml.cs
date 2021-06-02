ca0dcae3b3539115bdf8630d3112e573c045991f
trum <trumeetration@gmail.com>
2021-06-03 01:02:39 +0500

    [Started] Stats view

---
+++ Footer/Footer/Views/TabItemsContents/StatisticsPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microcharts;
+using SkiaSharp;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Footer.Views.TabItemsContents
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StatisticsPage : Grid
+    {
+        private List<ChartEntry> entries = new List<ChartEntry>()
+        {
+            new ChartEntry(10)
+            {
+                Color = SKColor.Parse("#FF7600")
+            },
+            new ChartEntry(5)
+            {
+                Color = SKColor.Parse("#FF7600")
+            },
+            new ChartEntry(30)
+            {
+                Color = SKColor.Parse("#FF7600")
+            },
+            new ChartEntry(25)
+            {
+                Color = SKColor.Parse("#FF7600")
+            },
+            new ChartEntry(20)
+            {
+                Color = SKColor.Parse("#FF7600")
+            },
+        };
+		public StatisticsPage ()
+        {
+			InitializeComponent ();
+            ChartView1.Chart = new LineChart() {Entries = entries};
+		}
+	}
+}
